5b3559c0747716301cc59e6f122e59e047ca8521
Edwin Chajon <junchajo@gmail.com>
2020-02-14 10:58:35 -0800

    project rouiting

--- data/router/actionsRouter.js
+++ data/router/actionsRouter.js
@@ -5,20 +5,18 @@ const router = express.Router();
 const Actions = require("../helpers/actionModel");
 
 
-
+// get all
 router.get("/", (req, res) => {
-    Actions.get()
-      .then(data => {
-        res.status(200).json(data);
-      })
-      .catch(error => {
-        console.log(error);
-        res.status(500).json({
-          errorMessage: "Error, actions could not be found."
-        });
-      });
+    Actions.get().then(data => {res.status(200).json(data)})
+           .catch(err => {res.status(500).json({Message: "Error, actions could not be found."})});
   });
 
+//get by id
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    Actions.get(id).then(data => {{res.status(200).json(data)}})
+           .catch(error => {res.status(500).json({Message: "actions cannot not be found"})});
+});
 
 
 
--- data/router/projectsRouter.js
+++ data/router/projectsRouter.js
@@ -5,28 +5,48 @@ const router = express.Router();
 const Projects = require("../helpers/projectModel");
 
 
-
+// get all
 router.get("/", (req, res) => {
-    Projects.get()
-      .then(data => {
-        res.status(200).json(data);
-      })
-      .catch(error => {
-        console.log(error);
-        res.status(500).json({
-          errorMessage: "Error retrieving projects."
-        });
-      });
+    Projects.get().then(data => {res.status(200).json(data)})
+            .catch(err => {res.status(500).json({Message: "Error getting projects."})});
   });
   
+//get by id
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    Projects.get(id).then(data => {res.status(200).json(data)})
+      .catch(err => {res.status(500).json({Message: "Error getting by id."})});
+});
+  
+//add project
+router.post("/", (req, res) => {
+  const project = req.body;
+  Projects.insert(project).then(proj => {
+            project.name && project.description ?
+              res.status(201).json(project)
+            : res.status(400).json({ Message: "need both name and description"})})
+          .catch(err => {res.status(500).json({Message: "Error saving this project"})});
+});
 
+//change project contents
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const project = req.body;
 
+  Projects.update(id, project).then(updated => {
+      project.name && project.description && project.completed ?
+              res.status(200).json({ updated })
+            : res.status(400).json({ errorMessage: "Projects could not be edited" })})
+          .catch(err => {res.status(500).json({Message: "The project information couldn't update"})});
+});
 
+//delete user
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
 
-
-
-
-
+  Projects.remove(id).then(deleted => {res.status(200).json({ deleted })})
+    .catch(err => {res.status(500).json({Message: "could not removed"})});
+});
 
 
 module.exports = router;
